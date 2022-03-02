9c478c79591c484d8cf8a6ed20a3d239601e5267
Luong NGUYEN <luongnv89@gmail.com>
2022-03-02 15:48:03 +0100

    first version

---
+++ README.md
@@ -0,0 +1,5 @@
+# eicar_virus
+
+EICAR virus testing application - for testing anti-malware application
+
+It does no harm to your device
---
+++ android/app/src/main/kotlin/com/example/eicar_virus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eicar_virus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
